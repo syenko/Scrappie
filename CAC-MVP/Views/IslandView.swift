e5a6cc4cd889fc37152520860c11329d3e41b0ec
Vir Shah <virn.shah@gmail.com>
2022-10-12 16:54:11 -0700

    everything on island except for actual island

--- CAC-MVP/Views/IslandView.swift
+++ CAC-MVP/Views/IslandView.swift
@@ -7,38 +7,78 @@
 
 import SwiftUI
 
+struct CircleButton: View {
+    let imageName: String
+    var body: some View {
+        ZStack {
+            Circle().frame(width: 55, height: 55, alignment: .center)
+            Circle().frame(width: 50, height: 50, alignment: .center)
+                .foregroundColor(.white)
+            Image(systemName: self.imageName)
+            
+        }
+    }
+}
+
 struct IslandView: View {
+    @State var showingStreaks: Bool = false
+    @State var showingHelp: Bool = false
+    
     var body: some View {
         ZStack {
             HStack {
                 Spacer()
-                VStack {
-                    ZStack {
-                        Circle().frame(width: 55, height: 55, alignment: .center)
-                        Circle().frame(width: 50, height: 50, alignment: .center)
-                            .foregroundColor(.white)
-                        Image(systemName: "number")
-
+                VStack(spacing: 10) {
+                    Button {
+                        showingStreaks.toggle()
+                    } label: {
+                        CircleButton(imageName: "number")
                     }
-                    ZStack {
-                        Circle().frame(width: 55, height: 55, alignment: .center)
-                        Circle().frame(width: 50, height: 50, alignment: .center)
-                                .foregroundColor(Color.white)
-                        Image(systemName: "gift")
-
-                    }
-                    ZStack {
-                        Circle().frame(width: 55, height: 55, alignment: .center)
-                        Circle().frame(width: 50, height: 50, alignment: .center)
-                                .foregroundColor(Color.white)
-                        Image(systemName: "questionmark")
-
+                    CircleButton(imageName: "gift")
+                    Button {
+                        showingHelp = true
+                    } label: {
+                        CircleButton(imageName: "questionmark")
                     }
                     Spacer()
                 }.padding(.trailing)
             }
             
             Text("Island")
+            if showingStreaks {
+                GroupBox {
+                    HStack {
+                        Text("Highest:")
+                        Spacer()
+                        Text("10").monospacedDigit()
+                            .font(.title)
+                    }
+                    Image(systemName: "chart.bar.xaxis")
+                        .font(.system(size: 100))
+                    HStack() {
+                        Spacer()
+                        CircleButton(imageName: "square.fill")
+                        Spacer()
+                        CircleButton(imageName: "play.fill")
+                        Spacer()
+                    }.scaleEffect(0.7)
+                } label: {
+                    HStack {
+                        Label("1 Week Streak!", systemImage: "flame")
+                            .font(.title2)
+                            .foregroundStyle(LinearGradient(colors: [.red,.orange], startPoint: .zero, endPoint: .trailing))
+                        Spacer()
+                        Button {
+                           showingStreaks = false
+                        } label: {
+                            Image(systemName: "x.circle")
+                                .padding()
+                        }
+                    }
+                }.frame(maxWidth:300)
+            }
+        }.popover(isPresented: $showingHelp) {
+            Text("Please contact your local officials for any help required.")
         }
     }
 }
