8ae14969972341dc199046402c113ea44902d443
Sabrina <59457991+syenko@users.noreply.github.com>
2022-10-30 02:03:42 -0700

    Add TabBar camera button menu
    This commit causes the camera button in the tab bar to show buttons which
    will represent the two actions that can be taken.

--- CAC-MVP/Views/ContentView.swift
+++ CAC-MVP/Views/ContentView.swift
@@ -11,32 +11,66 @@ import UIKit
 struct ContentView: View {
     @StateObject var viewController = ViewController()
     
+    // Source: https://stackoverflow.com/questions/60394201/tabbar-middle-button-utility-function-in-swiftui
+    @State private var selectedItem = 1
+    @State private var oldSelectedItem = 1
+    @State private var showActionSheet = false
     
     var body: some View {
-        TabView {
+        TabView(selection: $selectedItem) {
             IslandView()
                 .environmentObject(viewController)
                 .tabItem {
                     Image(systemName: "globe.americas.fill")
+                }
+                .tag(1)
+                .onAppear {
+                    self.oldSelectedItem = self.selectedItem
                 }
             MealsView()
                 .environmentObject(viewController)
                 .tabItem {
                     Image(systemName: "fork.knife")
                 }
-            CameraView()
+                .tag(2)
+                .onAppear {
+                    self.oldSelectedItem = self.selectedItem
+                }
+            IslandView()
+                .environmentObject(viewController)
                 .tabItem {
                     Image(systemName: "camera")
+                }
+                .tag(3)
+                .onAppear {
+                    self.selectedItem = self.oldSelectedItem
+                    self.showActionSheet.toggle()
                 }
             GroceriesView()
                 .tabItem {
                     Image(systemName: "bag.fill")
+                }
+                .tag(4)
+                .onAppear {
+                    self.oldSelectedItem = self.selectedItem
                 }
             StatsView()
                 .environmentObject(viewController)
                 .tabItem {
                     Image(systemName: "chart.xyaxis.line")
                 }
+                .tag(5)
+                .onAppear {
+                    self.oldSelectedItem = self.selectedItem
+                }
+        }
+        .confirmationDialog("", isPresented: $showActionSheet, titleVisibility: .hidden) {
+            Button("Scan Receipts") {
+                
+            }
+            Button("Scan Food") {
+                
+            }
         }
         .onAppear {
 //            UITabBar.appearance().scrollEdgeAppearance = UITabBarAppearance.init(idiom: .unspecified)
